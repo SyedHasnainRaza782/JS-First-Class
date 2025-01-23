af6a49e26120fd0984af9425ae364e3cd00b0dc0
SyedHasnainRaza782 <officialsyedhasnainraza034@gmail.com>
2025-01-23 21:06:31 +0500

    Add files via upload

---
+++ 1 Class Admin/app.js
@@ -0,0 +1,21 @@
+// // String declare karna
+ let greeting = "Hello, world!";
+//console.log("greeting" +" " + "Saylani");
+
+
+
+// let newGreeting = greeting.replace("world", "JavaScript");
+// console.log("Naya greeting: " + "newGreeting");
+
+
+
+
+
+
+var newGreeting = ( "Hello" +"world" +  "JavaScript");
+
+
+
+// String ko uppercase mein convert karna
+
+console.log(newGreeting.toString(889));
